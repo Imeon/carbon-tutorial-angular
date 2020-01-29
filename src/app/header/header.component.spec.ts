0d8f35e800f216cdf48f04a7ffeb1390ed73cd18
Imeon <35657294+Imeon@users.noreply.github.com>
2020-01-29 16:50:46 +0200

    Update header.component.spec.ts

--- src/app/header/header.component.spec.ts
+++ src/app/header/header.component.spec.ts
@@ -6,6 +6,8 @@ import { Notification20Module } from '@carbon/icons-angular/lib/notification/20'
 import { UserAvatar20Module } from '@carbon/icons-angular/lib/user--avatar/20';
 import { AppSwitcher20Module } from '@carbon/icons-angular/lib/app-switcher/20';
 
+import { UIShellModule } from 'carbon-components-angular/ui-shell/ui-shell.module';
+
 describe('HeaderComponent', () => {
 	let component: HeaderComponent;
 	let fixture: ComponentFixture<HeaderComponent>;
